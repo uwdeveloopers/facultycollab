bf1081c31a20bf23c6fa06ba7ef7dbede5f155cb
Cameron Bardell <cambardell3@gmail.com>
2019-10-01 18:56:35 -0400

    firebase initialization

--- facultycollab/facultycollab/AppDelegate.swift
+++ facultycollab/facultycollab/AppDelegate.swift
@@ -7,16 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+   var window: UIWindow?
+
+      func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
-    }
+      }
+    
 
     // MARK: UISceneSession Lifecycle
 
@@ -32,6 +38,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
+
 
