e120582dc78b889c4700513ac22a94d560d2e56e
Jason <j927chen@edu.uwaterloo.ca>
2019-10-28 23:17:14 -0400

    I still don't know if DashboardView runs properly because of the Firebase initialization errors.

--- facultycollab/facultycollab/DashboardView.swift
+++ facultycollab/facultycollab/DashboardView.swift
@@ -1,5 +1,7 @@
 //
-//  DashboardView: a work in progress by Jason. Sorry if it crashes :(
+//  DashView
+//
+//  A work-in-progress by Jason
 //
 //
 
@@ -8,54 +10,57 @@ import SwiftUI
 struct DashboardView: View {
     @State private var search: String = ""
     var body: some View {
-        VStack(spacing: 20) {
-            HStack {
-                Spacer()
-                Text("WATcollab")
-                    .font(.system(size: 50))
-                    .bold()
-                    .foregroundColor(.yellow)
-                Spacer()
-            }.background(Color.black)
-            Image("Colors")
-                .resizable()
-                .scaledToFit()
-            HStack {
-                Button( action: {}, label: { Image(systemName: "house") } )
-                Spacer()
-                Button( action: {}, label: {
-                    Image(systemName: "pencil")
-                    Text("Post")})
-            }.padding()
-            Divider()
-            Text("Explore")
-                .font(.system(size: 30))
-                .foregroundColor(.purple)
-            TextField("Search", text: $search)
-                .textFieldStyle(RoundedBorderTextFieldStyle()).padding()
-            TabView {
-                List {
-                    Text("Hello")
-                    Text("Darkness")
-                    Text("My")
-                }.tabItem({
-                    Image(systemName: "circle")
-                    Text("All")}).tag(0)
-                List {
-                    Text("Old")
-                    Text("Friend")
-                }.tabItem({
-                    Image(systemName: "plusminus")
-                    Text("Math")}).tag(1)
-                Text("Engineering Faculty").tabItem({
-                    Image(systemName: "gear")
-                    Text("Engineering")}).tag(2)
-                Text("Science Faculty").tabItem({
-                    Image(systemName: "tornado")
-                    Text("Science")}).tag(3)
-                Text("Arts Faculty").tabItem({
-                    Image(systemName: "paintbrush")
-                    Text("Arts")}).tag(4)
+        ZStack {
+            Color.yellow.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                HStack {
+                    Spacer()
+                    Text("WATcollab")
+                        .font(.system(size: 50))
+                        .bold()
+                        .foregroundColor(.yellow)
+                    Spacer()
+                }.background(Color.black)
+                Image("Colors")
+                    .resizable()
+                    .scaledToFit()
+                HStack {
+                    Button( action: {}, label: { Image(systemName: "house") } )
+                    Spacer()
+                    Button( action: {}, label: {
+                        Image(systemName: "pencil")
+                        Text("Post")})
+                }.padding()
+                Divider()
+                Text("Explore")
+                    .font(.system(size: 30))
+                    .foregroundColor(.purple)
+                TextField("Search", text: $search)
+                    .textFieldStyle(RoundedBorderTextFieldStyle()).padding()
+                TabView {
+                    List {
+                        Text("Hello")
+                        Text("Darkness")
+                        Text("My")
+                    }.tabItem({
+                        Image(systemName: "circle")
+                        Text("All")}).tag(0)
+                    List {
+                        Text("Cool")
+                        Text("Nice")
+                    }.tabItem({
+                        Image(systemName: "plusminus")
+                        Text("Math")}).tag(1)
+                    Text("Engineering Faculty").tabItem({
+                        Image(systemName: "gear")
+                        Text("Engineering")}).tag(2)
+                    Text("Science Faculty").tabItem({
+                        Image(systemName: "tornado")
+                        Text("Science")}).tag(3)
+                    Text("Arts Faculty").tabItem({
+                        Image(systemName: "paintbrush")
+                        Text("Arts")}).tag(4)
+                }
             }
         }
     }
